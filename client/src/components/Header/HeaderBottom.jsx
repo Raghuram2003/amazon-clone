3569b114b7cde93c0a8bb4f76f3f04f94c11fec3
Raghuram2003 <destrobroxd@gmail.com>
2024-01-19 00:56:55 +0530

    Created SideBar and Banner Carousel

--- client/src/components/Header/HeaderBottom.jsx
+++ client/src/components/Header/HeaderBottom.jsx
@@ -1,30 +1,76 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
-
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import SideNav from "./SideNav";
+import CloseIcon from '@mui/icons-material/Close';
+import {motion} from "framer-motion"
+import { useRef } from "react";
 
 const HeaderBottom = () => {
-  const [navBar,setNavBar] = useState(false)
+  const [sideBar, setSideBar] = useState(false);
+  const ref = useRef()
+  useEffect(()=>{
+    document.body.addEventListener("click",(ev)=>{
+      console.log(ev.target)
+      if(ev.target.contains(ref.current)){
+        setSideBar(false)
+      }
+    })
+  },[ref,sideBar])
   return (
     <div className="w-full h-[36px] px-4 bg-amazon_light text-white flex items-center gap-2 relative">
       <ul className="flex items-center text-sm tracking-wide">
-        <li onClick={()=>setNavBar(true)} className="headerHover flex items-center gap-1"><MenuIcon/>All</li>
+        <li
+          onClick={() => setSideBar(true)}
+          className="headerHover flex items-center gap-1"
+        >
+          <MenuIcon />
+          All
+        </li>
         <li className="headerHover">Todays Deals</li>
         <li className="headerHover">Customer Service</li>
         <li className="headerHover">Gift Cards</li>
-        <li className="header Hover">Registry</li>
+        <li className="headerHover">Registry</li>
         <li className="headerHover">Sell</li>
       </ul>
-      {navBar && (
+      {sideBar && (
         <div className="w-full h-screen text-black fixed top-0 left-0 bg-amazon_blue bg-opacity-50">
           <div className="relative w-full h-full">
-              <div className="w-[350px] h-full bg-white border border-black ">
-                  <div className="w-full bg-amazon_light text-white py-2 px-6 flex items-center gap-4">
-                      <AccountCircleIcon/> 
-                      <h3 className="font-titleFont font-bold text-lg tracking-wide">Hello, Sign In</h3>
-                  </div>
+            <motion.div ref={ref} initial={{x:-500,opacity:0}} animate={{x:0,opacity:1}} transition={{duration:.5}} className="w-[350px] h-full bg-white border border-black ">
+              <div className="w-full bg-amazon_light text-white py-2 px-6 flex items-center gap-4">
+                <AccountCircleIcon />
+                <h3 className="font-titleFont font-bold text-lg tracking-wide">
+                  Hello, Sign In
+                </h3>
               </div>
+              <SideNav
+                title="Digital Content & Devices"
+                firstItem="Amazon Music"
+                secondItem="Kindle E-readers & Books"
+                thirdItem="Amazon Appstore"
+              />
+              <SideNav
+                title="Shop By Department"
+                firstItem="Electronics"
+                secondItem="Computers"
+                thirdItem="Smart Home"
+              />
+              <SideNav
+                title="Programs & Features"
+                firstItem="Gift Cards"
+                secondItem="Amazon live"
+                thirdItem="International Shopping"
+              />
+              <SideNav
+                title="Help & Settings"
+                firstItem="Your Account"
+                secondItem="Customer Service"
+                thirdItem="Contact us"
+              />
+              <span onClick={()=>setSideBar(false)} className="absolute top-0 left-[360px] bg-gray-200 text-black flex items-center justify-center border hover:bg-red-500 hover:text-white duration-300 p-2"><CloseIcon/>
+              </span>
+            </motion.div>
+            
           </div>
         </div>
       )}
--- client/src/components/Header/SideNav.jsx
+++ client/src/components/Header/SideNav.jsx
@@ -1,10 +1,34 @@
-import CloseIcon from "@mui/icons-material/Close"
-const SideNav = ({navBar,setNavBar}) => {
-  return (
-    <div className="absolute">
-        SideNav      
-    </div>
-  )
-}
 
-export default SideNav
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+// eslint-disable-next-line react/prop-types
+const SideNav = ({title,firstItem,secondItem,thirdItem}) => {
+  return (
+    <div className='py-3 border-b-[1px] border-b-gray-300'>
+      <h3 className="px-6 mb-1 font-titleFont font-semibold text-lg">
+        {title}
+      </h3>
+      <ul className='text-sm'>
+        <li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+          {firstItem}
+          <span>
+            <KeyboardArrowRightIcon />
+          </span>
+        </li>
+        <li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+          {secondItem}
+          <span>
+            <KeyboardArrowRightIcon />
+          </span>
+        </li>
+        <li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+          {thirdItem}
+          <span>
+            <KeyboardArrowRightIcon />
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SideNav;
